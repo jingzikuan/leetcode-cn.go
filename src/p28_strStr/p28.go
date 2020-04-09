b7e81ac815f0f15d08a9efb864c2927a31b7322b
jingzikuan <jingzk@kyong.net>
2020-04-09 09:33:37 +0800

    优化 执行用时 : 0 ms , 在所有 Go 提交中击败了 100.00% 的用户 内存消耗 : 2.3 MB , 在所有 Go 提交中击败了 100.00% 的用户

--- src/p28_strStr/p28.go
+++ src/p28_strStr/p28.go
@@ -27,3 +27,24 @@ func strStr(haystack string, needle string) int {
 
 	return -1
 }
+
+/**
+执行用时 : 0 ms , 在所有 Go 提交中击败了 100.00% 的用户
+内存消耗 : 2.3 MB , 在所有 Go 提交中击败了 100.00% 的用户
+*/
+func strStr2(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+	sublen := len(haystack) - len(needle)
+	if sublen < 0 {
+		return -1
+	}
+	for i := 0; i <= sublen; i++ {
+		newstr := haystack[i : i+len(needle)]
+		if newstr == needle {
+			return i
+		}
+	}
+	return -1
+}
--- src/p28_strStr/p28_test.go
+++ src/p28_strStr/p28_test.go
@@ -1,10 +1,21 @@
 package p28_strStr
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestStrStr(t *testing.T) {
 	//test
 	if strStr("abcdef", "bc") != 1 {
+		t.FailNow()
+	}
+}
+
+func TestStrStr2(t *testing.T) {
+	res := strStr2("a", "a")
+	fmt.Println(res)
+	if res != 0 {
 		t.FailNow()
 	}
 }
